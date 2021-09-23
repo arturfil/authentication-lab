ab71e9375d92840c17c1af3a432d1961aa041582
thomaskno <thomas.kno@yahoo.com>
2021-09-23 09:08:23 -0400

    Add check middleware to signup

--- routes/user.js
+++ routes/user.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
-const {
-  userSignup,
-  userLogin
-} = require("../controllers/userController");
+const { check } = require('express-validator')
+const { validateFields } = require('../middlewares/validateField')
+
+const { userSignup, userLogin } = require("../controllers/userController");
 
 
 // POST - Sign up
-router.post("/signup", userSignup)
+router.post("/signup",[
+  check("name", "Name field is required").not().isEmpty(),
+  check("email", "Email field is required").not().isEmpty(),
+  check("password", "Password must be 8 charaacters long").isLength({min: 8}),
+  validateFields
+], userSignup)
 
 // Login
 router.post("/login", userLogin)
